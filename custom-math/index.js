f7e60f024d8d504973064ad7fa9c85b03b274a86
karla galvez <karla.galvez.m@hotmail.com>
2019-05-31 21:20:32 -0500

    mensaje

---
+++ custom-math/index.js
@@ -0,0 +1,9 @@
+
+const suma = require('./suma')
+
+
+
+const resta = require('./resta')
+
+module.exports.resta = resta
+module.exports.suma = suma
---
+++ sample-math.js
@@ -0,0 +1,8 @@
+const customMath = require('./custom-math')
+
+console.log (customMath.suma(2,10))
+
+console.log("resta: " + customMath.resta(2,1))
+
+
+
